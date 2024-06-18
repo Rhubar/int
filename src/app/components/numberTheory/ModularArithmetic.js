af24833a67ff636035f67dc640f481340a186333
Alexander Szwabowicz <szwab.alexander@gmail.com>
2024-06-18 22:16:43 +0100

    Modular Arithmetic halfway

--- src/app/components/numberTheory/ModularArithmetic.js
+++ src/app/components/numberTheory/ModularArithmetic.js
@@ -1,14 +1,52 @@
-import '../styles/mod.css';
+import "../styles/mod.css";
+import Accordion from "../Accordion";
 
-function ModularArithmetic(props){
+function ModularArithmetic(props) {
+  // accordion data
+  const titleWhat = "What is it?";
+  const contentWhat = (
+    <p>
+      Modular arithmetic is a type of arithmetic where numbers "wrap around" after they
+      reach a certain value — the modulus. For example, think of an analogue
+      clock. 3 hours after 11 is 2, since after reaching 12 it resets to 0.
+    </p>
+  );
+  const titleWhy = "Why is it important?";
+  const contentWhy = (
+    <p>
+      Modular arithmetic helps manage the cyclical nature of many processes
+      <br />
+      <br />
+      Cryptography: It's fundamental for algorithms in encryption
+      <br />
+      <br />
+      Hash functions: These use modular arithmetic to convert large datasets
+      into fixed-size integers known as hash values
+      <br />
+      <br />
+      Graphics: To create patterns or manage circular arrays
+      <br />
+      <br />
+      Algorithms: To keep integers within a fixed range to prevent overflow
+    </p>
+  );
 
-    if(props.topic === "mod") {
-        return(
-            <div className="modDiv">
-                <h1>coming soon</h1>
-            </div>
-        );
-    }
+//  to do: interactive - find modulus of two numbers? 
+  if (props.topic === "mod") {
+    return (
+      <div className="modDiv">
+        <div className="modBackground"></div>
+        <div className="modBackColor"></div>
+        <h1>Modular Arithmetic</h1>
+        <div className="modAccordion">
+          <Accordion title={titleWhat} content={contentWhat} />
+        </div>
+        <div className="modAccordion">
+          <Accordion title={titleWhy} content={contentWhy} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ModularArithmetic;
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
   <main className={styles.main}>
     <h2>Welcome to the Interactive Number Theory Playground!</h2>
     <h1>Playground Under Construction</h1>
-    <h2>Number Theory App halfway done</h2>
+    <h2>The Number Theory App is mostly done</h2>
+    <h2>Set Operations are yet to be started</h2>
     <p>This is a project built in React.js using the Next.js framework</p>
     <p>It is a cool web-based interactive tool that allows users to explore various number theory concepts and set operations</p>
   </main>
